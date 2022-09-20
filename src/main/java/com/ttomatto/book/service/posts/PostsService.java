6c4225e9965b8c92910afda8af1ed632764ca0e2
ttomatto <hoyoyogood@gmail.com>
2022-09-20 18:24:13 +0900

    4장 mustache로 페이지 구현 스프링부트 2.7 버전의 경우 mustache 사용 시 한글 깨짐 문제 있어 yml 파일에 옵션 추가 필요

--- src/main/java/com/ttomatto/book/service/posts/PostsService.java
+++ src/main/java/com/ttomatto/book/service/posts/PostsService.java
@@ -2,13 +2,16 @@ package com.ttomatto.book.service.posts;
 
 import com.ttomatto.book.domain.posts.Posts;
 import com.ttomatto.book.domain.posts.PostsRepository;
+import com.ttomatto.book.web.dto.PostsListResponseDto;
 import com.ttomatto.book.web.dto.PostsResponseDto;
 import com.ttomatto.book.web.dto.PostsSaveRequestDto;
 import com.ttomatto.book.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -35,5 +38,18 @@ public class PostsService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id ="+id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id = "+id));
+        postsRepository.delete(posts);
     }
 }
